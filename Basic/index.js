13b20f2a4c2e38f18c6cb32c1c69c0c0b1b82ac4
Syed Zain Rashid <55804149+Zain1298@users.noreply.github.com>
2020-07-08 16:38:41 +0500

    update

---
+++ Basic/index.js
@@ -0,0 +1,32 @@
+// Prints Hello world
+alert("Hello World");
+console.log("Hello World");
+
+//Prints name and age on console
+var name = 'Zain';
+var age = 22;
+console.log(name);
+console.log(age);
+
+// Prints null on the console.
+var name = null; // null
+console.log(name);
+console.log(typeof name);
+
+//Declares undefined 
+var nationality;  // undefined
+console.log(nationality);
+
+//typoeof is used to show the type of variable
+var isFreepaid = true;
+console.log(isFreepaid);
+console.log(typeof isFreepaid);
+
+//Add two numbers and print on console and alert
+var a = 10;
+var b = 10;
+
+var c = a + b ;
+alert(c);
+console.log(c);
+console.log(typeof c);
